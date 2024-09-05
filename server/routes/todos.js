559226bf3b54bde7c7dba23cf22b608dcff792c8
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-05 16:22:48 +0530

    map routes with controllers

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -5,9 +5,17 @@ const router = express.Router();
 
 // import controller
 const {createTodo} = require('../controllers/createTodo');
+const {getTodos, getTodoById} = require('../controllers/getTodo');
+const {updateTodo} = require('../controllers/updateTodo');
+const {deleteTodo} = require('../controllers/deleteTodo');
+
 
 // map the route with the controller
 router.post('/createTodo', createTodo)
+router.get('/getTodos', getTodos);
+router.get('/getTodoById/:id', getTodoById);
+router.put('/updateTodo/:id', updateTodo)
+router.delete('/deleteTodo/:id', deleteTodo)
 
 
 // export the router 
