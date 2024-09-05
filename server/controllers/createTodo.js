4372790ae77c86c18f3f808002aaf3271f04567e
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-05 09:15:43 +0530

    define logic of createTodo

---
+++ server/controllers/createTodo.js
@@ -0,0 +1,37 @@
+const Todo = require('../models/todo.model')
+
+// define route handler
+exports.createTodo = async (req, res) => {
+    
+    try {
+        // extract title, description from req ki body
+        const {title, description} = req.body;
+        //create a new Todo and insert in DB
+        const todo = await Todo.create({title, description});
+        // send success response if todo created
+        if(todo) {
+            res.status(200).json(
+                {
+                    success: true,
+                    data: todo,
+                    message: 'Entry created successfully'
+                }
+            )
+        }
+
+    } catch (error) {
+        // send failure message
+        console.log(error);
+        res.status(500).json(
+            {
+                success: false,
+                data: 'internal server error',
+                message: error.message,
+            }
+        )
+    }
+
+}
+
+// export route handler
+// module.exports = createTodo;
