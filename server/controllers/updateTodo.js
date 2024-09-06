4ce401240a327397c1a130303373ea372091fb7e
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-06 21:31:40 +0530

    remove description property from controller

--- server/controllers/updateTodo.js
+++ server/controllers/updateTodo.js
@@ -5,12 +5,12 @@ exports.updateTodo = async (req, res) => {
         // extract todo id from params
         const id = req.params.id;
         // extract title-desc from req body
-        const {title, description} = req.body;
+        const {title} = req.body;
 
         // find todo by id and update in db
         const updatedTodo = await Todo.findByIdAndUpdate(
                                             id,
-                                            {title, description, updatedAt: Date.now()},
+                                            {title},
                                             {new : true}
         )
         if (!updatedTodo) {
